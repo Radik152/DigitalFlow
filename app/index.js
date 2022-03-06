b840708f48cb3e8c290c72410d24fee8a07c6614
Radik152 <rezyabkinradik@gmail.com>
2022-03-06 15:21:46 +0300

    сделал выпадающее меню и блок о компании

--- app/index.js
+++ app/index.js
@@ -3,11 +3,20 @@ require('./style.css');
 const burgerShowMenu = document.querySelector('.menuBurger-header');
 const menu = document.querySelector('.menu');
 const burgerHideMenu = document.querySelector('.button-hide-menu');
+const page = document.querySelector('.page');
 
 burgerShowMenu.onclick = function() {
     menu.classList.add('showMenu');
+    menu.style.display = 'block';
+    page.classList.add('no-scroll');
 }
 
 burgerHideMenu.onclick = function() {
-    menu.classList.remove('showMenu');
+    menu.classList.add('againHide');
+    page.classList.remove('no-scroll');
+    setTimeout(()=> {
+        menu.style.display = 'none';
+        menu.classList.remove('showMenu');
+        menu.classList.remove('againHide');
+    }, 1000)
 }
