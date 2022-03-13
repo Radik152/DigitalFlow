aed073bcaa09c0708d2f1d6b49b115fd6022bbf3
Radik152 <rezyabkinradik@gmail.com>
2022-03-13 13:28:28 +0300

    offer block start

--- app/index.js
+++ app/index.js
@@ -4,10 +4,26 @@ const burgerShowMenu = document.querySelector('.menuBurger-header');
 const menu = document.querySelector('.menu');
 const burgerHideMenu = document.querySelector('.button-hide-menu');
 const page = document.querySelector('.page');
+const dots = document.getElementsByClassName('dot-item');
+const offerBlock = document.querySelector('.offer-list-container');
+
+const offerItems = [
+    {id: 1, class: "offer-high-level", title: "Высокий уровень исполнения "},
+    {id: 2, class: "offer-awards", title: "Гарантируем качество работы"},
+    {id: 3, class: "offer-stopwatch", title: "Выполним работу быстро"},
+    {id: 4, class: "offer-mail", title: "Обеспечим поток заявок"},
+    {id: 5, class: "offer-automat", title: "Автоматизируем работу "},
+    {id: 6, class: "offer-design", title: "Разработаем уникальный дизайн"},
+    {id: 7, class: "offer-code", title: "Напишем чистый код"},
+    {id: 8, class: "offer-advertisement", title: "Контекстная реклама"},
+    {id: 9, class: "offer-advertisement_2", title: "Третированная реклама"},
+    {id: 10, class: "offer-seo", title: "SEO продвижение"}
+]
+
 
 burgerShowMenu.onclick = function() {
-    menu.classList.add('showMenu');
     menu.style.display = 'block';
+    menu.classList.add('showMenu');
     page.classList.add('no-scroll');
 }
 
@@ -20,3 +36,19 @@ burgerHideMenu.onclick = function() {
         menu.classList.remove('againHide');
     }, 1000)
 }
+
+offerItems.map(item => {
+    const divContainer = document.createElement('div');
+    const divIcon = document.createElement('div');
+    const spanTitle = document.createElement('span');
+    spanTitle.classList.add('offer-item-title');
+    spanTitle.innerHTML = item.title;
+    divIcon.classList.add(`${item.class}`);
+    divContainer.classList.add(`offer-item`);
+    if(item.id % 5 !== 0) {
+        divContainer.classList.add('offer-item-right-border');
+    }
+    divContainer.appendChild(divIcon);
+    divContainer.appendChild(spanTitle);
+    offerBlock.appendChild(divContainer);
+})
